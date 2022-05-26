8919a50b81d88f6b97aa473bc29835cb75fdb54a
Vinicius Lima <vinicius.lima.brasil1@gmail.com>
2022-05-26 12:03:09 -0300

    hash da senha

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/users/create", (req, res) => {
   res.render("admin/users/create");
@@ -9,12 +10,27 @@ router.get("/admin/users/create", (req, res) => {
 router.post("/admin/users/save", (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
-
-  User.create({
-    username: username,
-    password: password,
-  }).then(() => {
-    res.redirect("/admin/users");
+  let salt = bcrypt.genSaltSync(16);
+  let hash = bcrypt.hashSync(password, salt);
+  User.findOne({
+    where: {
+      username: username,
+    },
+  }).then((exists) => {
+    if (exists) {
+      res.redirect("/admin/users/create");
+    } else {
+      User.create({
+        username: username,
+        password: hash,
+      })
+        .then(() => {
+          res.redirect("/admin/users");
+        })
+        .catch((err) => {
+          res.send("Ocorreu um erro!");
+        });
+    }
   });
 });
 
